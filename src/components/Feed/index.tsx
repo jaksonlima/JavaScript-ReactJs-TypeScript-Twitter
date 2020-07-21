a635fbdfc29ba011aeeba4b4704f736b97b3e3af
jaksonsneider <jaksonsneider@gmail.com>
2020-07-20 23:00:57 -0300

    Feed | Tweet

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Tab, Tweets } from './styles';
+import Tweet from '../Tweet'
+
+const Feed: React.FC = () => {
+    return (
+        <Container>
+            <Tab>Tweets</Tab>
+
+            <Tweets>
+                <Tweet/>
+                <Tweet/>
+            </Tweets>
+        </Container>
+    )
+}
+
+export default Feed;
