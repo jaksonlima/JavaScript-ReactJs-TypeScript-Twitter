1db789dc796386083a329a08b78fe3dd94ce09c7
jaksonsneider <jaksonsneider@gmail.com>
2020-07-21 23:20:55 -0300

    Tweet | Icon

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import { Container } from './styles';
+import { Description } from 'styled-icons/material';
+import { Body, CommentIcon, Container, Dot, Header, Icons, ImageContent, LikeIcon, Retweeted, RetweetIcon, RocketseatIcon, Status } from './styles';
 
 const Tweet: React.FC = () => {
     return (
         <Container>
+            <Retweeted>
+                <RocketseatIcon />
+                Voce retweetou
+            </Retweeted>
+
+            <Body>
+                <Header>
+                    <strong>Rocketseat</strong>
+                    <strong>@rocketseat</strong>
+                    <Dot />
+                    <time>27 de jun</time>
+                </Header>
+
+                <Description>Foguete não tem ré</Description>
+
+                <ImageContent />
+
+                <Icons>
+                    <Status>
+                        <CommentIcon />
+                        18
+                    </Status>
+                    <Status>
+                        <RetweetIcon />
+                        18
+                    </Status>
+                    <Status>
+                        <LikeIcon />
+                        18
+                    </Status>
+                </Icons>
+            </Body>
         </Container>
     )
 }
--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -13,3 +13,5 @@ export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
 
 export { Chat } from 'styled-icons/heroicons-outline';
+
+export { RocketseatIcon as Rocketseat } from '../components/Rocketseat'
