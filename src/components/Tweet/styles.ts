6f8b2dbfb040c87a7bbabe1709a04209304fdcd0
jaksonsneider <jaksonsneider@gmail.com>
2020-07-25 20:15:32 -0300

    Tweet

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Chat, Rocketseat, Favorite } from '../../styles/Icons';
 
 
@@ -85,10 +85,81 @@ export const Dot = styled.div`
     height: 2px;
     margin: 0 10px;
 `;
-export const Description = styled.div``;
-export const ImageContent = styled.div``;
-export const Icons = styled.div``;
-export const Status = styled.div``;
-export const CommentIcon = styled.div``;
-export const RetweetIcon = styled.div``;
-export const LikeIcon = styled.div``;
+export const Description = styled.p`
+    font-size: 14px;
+    margin-top: 4px;
+`;
+export const ImageContent = styled.div`
+    margin-top: 12px;
+    width: 100%;
+    height: min(258px, max(175px, 41vw));
+
+    background: var(--outline);
+    border-radius: 14px;
+
+    cursor: pointer;
+
+    &:hover {
+        opacity: .7;
+    }
+`;
+export const Icons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 11px auto 0;
+    width: 100%;
+
+    @media(min-width: 330px){
+        width: 63%;
+    }
+
+    > div {
+        cursor: pointer;
+
+        &:hover{
+            opacity: .7;
+        }
+    }
+`;
+export const Status = styled.div`
+    display: flex;
+    align-items: center;
+
+    font-size: 14px;
+
+    > svg {
+        margin-right: 5px;
+    }
+
+    &:nth-child(1) {
+        &, > svg path {
+            color: var(--gray);
+        }
+    }
+
+    &:nth-child(2) {
+        color: var(--retweet);
+
+        >svg path {
+            fill: var(--retweet);
+        }
+    }
+
+    &:nth-child(3) {
+        color: var(--like);
+
+        > svg {
+            fill: var(--like);
+        }
+    }
+`;
+
+const iconCss = css`
+    width: 19px;
+    height: 19px;
+`;
+export const CommentIcon = styled(Chat)`${iconCss}`;
+export const RetweetIcon = styled(Rocketseat)`${iconCss}`;
+export const LikeIcon = styled(Favorite)`${iconCss}`;
